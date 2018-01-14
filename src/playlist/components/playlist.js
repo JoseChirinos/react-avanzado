d4a490c0250e2c540ecb900cf39a6c1a3d046090
JoseChirinos <josealexcba@gmail.com>
2018-01-14 19:28:10 -0400

    desafio de agregar botones terminado, y leccion terminada

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -2,6 +2,10 @@
 import React from 'react';
 // Modules
 import Media from './media';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
+import FullScreen from '../../icons/components/fullscreen';
 // Styles
 import './playlist.css';
 
@@ -17,6 +21,24 @@ function PlayList(props){
 	const playlist = props.data.categories[0].playlist;
 	return (
 		<div className="Playlist">
+			<div className="Controls">
+				<Play
+					size={50}
+					color="red"
+				/>
+				<Pause
+					size={50}
+					color="red"
+				/>
+				<Volume
+					size={50}
+					color="red"
+				/>
+				<FullScreen
+					size={50}
+					color="red"
+				/>
+			</div>
 			{
 				playlist.map((item) => {					
 					return <Media
