1cb49d9b3183c313c80df7a928206a6ea0c69f21
JoseChirinos <josealexcba@gmail.com>
2018-01-15 00:06:42 -0400

    nueva estructura implementada, webpack modificado: los entry points

---
+++ src/entries/home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import Home from '../pages/containers/home';
+
+const app = document.getElementById('app');
+render(<Home/>,app);
---
+++ src/pages/containers/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HomeLayout from '../components/home-layout';
+class Home extends Component {
+	render(){
+		return (
+			<HomeLayout>
+				<h1>Hola Mundo React</h1> 
+			</HomeLayout>
+		);
+	}
+}
+
+export default Home;
