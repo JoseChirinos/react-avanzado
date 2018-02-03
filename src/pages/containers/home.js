c69d511d216f057042b45baac3f8f703a6e1e482
JoseChirinos <josealexcba@gmail.com>
2018-02-03 18:27:49 -0400

    new component added - related ready

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
+import Related from '../components/related-layout';
 class Home extends Component {
 	render(){
 		return (
 			<HomeLayout>
+				<Related/>
 				<Categories categories={ this.props.data.categories } />
 			</HomeLayout>
 		);
