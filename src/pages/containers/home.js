2ce04d66fd3fba61ebf08e53d6f6e31c35cdf945
JoseChirinos <josealexcba@gmail.com>
2018-02-10 21:15:31 -0400

    portales en reactjs

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
 import Related from '../components/related-layout';
+import ModalContainer from '../../widgets/containers/modal-container';
+
 class Home extends Component {
 	render(){
 		return (
 			<HomeLayout>
 				<Related/>
 				<Categories categories={ this.props.data.categories } />
+				<ModalContainer>
+					<h1>Q ondaaaaa </h1>
+				</ModalContainer>
 			</HomeLayout>
 		);
 	}
